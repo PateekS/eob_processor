52121706c9dc0ce798c08e56a234141ad22797da
Prateek Soni <71419411+PateekS@users.noreply.github.com>
2024-11-21 04:09:58 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Update package lists
-apt-get update
-
-# Install Poppler utils for Linux
-apt-get install -y poppler-utils
-# Install Tesseract OCR for Linux
-apt-get install -y tesseract-ocr
-
